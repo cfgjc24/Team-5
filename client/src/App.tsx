c383a51e04cd7e976a2923e058631e235d2d698b
Lauren Lascano <laurenlascano26@gmail.com>
2024-09-20 19:41:24 -0400

    moved login changes from app.tsx to login.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login.tsx'
+
 function App() {
-  let [userName, setUserName] = useState<string>('')
-  let [password, setPassword] = useState<string>('')
   return (
     <>
-      <div>
-      </div>
-      <h1>LoadStar</h1>
-      <div className="card">
-    <h3>UserName:</h3> 
-    <input value={userName}></input>
-    <h3>Password:</h3>
-    <input value={password}></input>
-    </div>
-    <button>Login</button>
+     <Login></Login> 
      
     </>
   )
--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -3,29 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-export default function App() {
-  const [count, setCount] = useState(0)
-
+export default function Login() {
+  let [userName, setUserName] = useState<string>('')
+  let [password, setPassword] = useState<string>('')
   return (
     <>
-    <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Load Star</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <nav >LoadStar Dashboard</nav>
-    <button>UserName:</button>
-    <button>Password:</button>
+    <div>
+      </div>
+      <h1>LoadStar</h1>
+      <div className="card">
+    <h3>UserName:</h3> 
+    <input value={userName}></input>
+    <h3>Password:</h3>
+    <input value={password}></input>
+    </div>
     <button>Login</button>
-    <script type="module" src="/src/main.tsx"></script>
-  </body>
-       
 
     </>
   )
 }
 
-export default Login
+
