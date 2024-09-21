14cc78e19fec620d1902ba6e5c7fcfb1835fb874
DiyaTomar <diyastomar@gmail.com>
2024-09-20 20:44:26 -0400

    add nextui tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,17 +5,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Staff from "./Staff";
 import Supervisor from "./Supervisor";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/staff" element={<Staff />} />
-          <Route path="/supervisor" element={<Supervisor />} />
-        </Routes>
-      </Router>
+      <NextUIProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/staff" element={<Staff />} />
+            <Route path="/supervisor" element={<Supervisor />} />
+          </Routes>
+        </Router>
+      </NextUIProvider>
     </>
   );
 }
