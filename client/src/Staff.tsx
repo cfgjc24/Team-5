f920dbe1e015c86621a7e11e4cf8a4ff58de8f9a
Xiaomin Liu <xl4624@nyu.edu>
2024-09-21 01:20:28 -0400

    Fix merge conflicts

--- client/src/Staff.tsx
+++ client/src/Staff.tsx
@@ -55,41 +55,6 @@ function Staff() {
     }
     ));
   }
-<<<<<<< HEAD
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center h-screen">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-x-11 md:gap-y-8">
-                    <Button color="success" className="w-80 md:w-70" onClick={() => handleButtonClick('good')}>
-                        Great
-                    </Button>
-                    <Button color="primary" className="w-80 md:w-70" onClick={() => handleButtonClick('extend')}>
-                        Extend Time
-                    </Button>
-                    <Button color="warning" className="w-80 md:w-70" onClick={() => handleButtonClick('emergency')}>
-                        Emergency
-                    </Button>
-                    <Button color="danger" className="w-80 md:w-70" onClick={() => handleButtonClick('sos')}>
-                        SOS
-                    </Button>
-                </div>
-                <input
-                    type="text"
-                    id="comment"
-                    name="comment"
-                    value={formData.comment}
-                    onChange={handleInputChange}
-                    className="mt-4"
-                />
-                {/* Submit Button */}
-                <input type="submit" value="Submit" className="mt-2" />
-                <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg mt-4" onClick={handleCheckOut}>
-                    Clock Out
-                </Button>
-            </form>
-        </>
-    );
-=======
 
   return (
     <>
@@ -139,7 +104,6 @@ function Staff() {
       </form>
     </>
   )
->>>>>>> 610345a (Make some progress on the formatting of the supervisor page)
 }
 
 export default Staff;
