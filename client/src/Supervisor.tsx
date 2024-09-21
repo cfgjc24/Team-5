1b89b5a51f9da1364f3203352b02d17e91dffa60
DiyaTomar <diyastomar@gmail.com>
2024-09-21 00:17:26 -0400

    remove MarkerList

--- client/src/Supervisor.tsx
+++ client/src/Supervisor.tsx
@@ -1,5 +1,4 @@
 import Map from "./supervisor/components/Map";
-import MarkerList from "./supervisor/components/MarkerList";
 import Notifications from "./supervisor/components/Notifications";
 
 export default function Supervisor() {
