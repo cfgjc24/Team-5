024073b5920097ffb00fec90e5b985ea49f86fd8
Xiaomin Liu <xl4624@nyu.edu>
2024-09-21 00:37:53 -0400

    Add Sidebar to Supervisor view

--- client/src/Supervisor.tsx
+++ client/src/Supervisor.tsx
@@ -1,10 +1,11 @@
 import Map from "./supervisor/components/Map";
 import Notifications from "./supervisor/components/Notifications";
+import Sidebar from "./supervisor/components/Sidebar";
 
 export default function Supervisor() {
   return (
     <>
-      <Map /> <Notifications />
+      <Map /><Sidebar /><Notifications />
     </>
   );
 }
