1d6788238298b70f4ee857f888a37bd48a1775b7
DiyaTomar <diyastomar@gmail.com>
2024-09-20 23:54:51 -0400

    remove MarkerList from displaying on Supervisor page

--- client/src/Supervisor.tsx
+++ client/src/Supervisor.tsx
@@ -1,9 +1,8 @@
 import Map from "./supervisor/components/Map";
-import MarkerList from "./supervisor/components/MarkerList";
 export default function Supervisor() {
   return (
     <>
-      <Map /> <MarkerList />
+      <Map />
     </>
   );
 }
--- client/src/supervisor/components/Map.tsx
+++ client/src/supervisor/components/Map.tsx
@@ -1,6 +1,6 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useState, useEffect } from "react";
-import { getMarkers } from "../../config/config" ;
+import { getMarkers } from "../../config/config.tsx";
 
 // Load API key from environment variable
 const mapKey = import.meta.env.VITE_MAP_API_KEY;
@@ -20,17 +20,15 @@ const locations = [
   { lat: 38.9, lng: -77.04 },
 ];
 
-
 export default function Map() {
-  const [markerList, setMarkerList] = useState([{}]) ;
+  const [markerList, setMarkerList] = useState([{}]);
 
   useEffect(() => {
-    getMarkers().then((value: any []) => {
-        // console.log(value) ;
-        setMarkerList(value) ;
-    })
-    
-},[])
+    getMarkers().then((value: any[]) => {
+      // console.log(value) ;
+      setMarkerList(value);
+    });
+  }, []);
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: mapKey,
@@ -42,11 +40,20 @@ export default function Map() {
   return (
     <GoogleMap mapContainerStyle={mapContainerStyle} zoom={10} center={center}>
       <Marker position={center} />
-      {markerList.map((marker => (
-        !("active" in marker) ? <></> : (
-        // <Marker key="a" position={marker} />
-          <Marker key="a" position={ {lat: (marker.coordinates.latitude), lng: (marker.coordinates.longitude)}} />
-      ))))}
+      {markerList.map((marker) =>
+        !("active" in marker) ? (
+          <></>
+        ) : (
+          // <Marker key="a" position={marker} />
+          <Marker
+            key="a"
+            position={{
+              lat: marker.coordinates.latitude,
+              lng: marker.coordinates.longitude,
+            }}
+          />
+        )
+      )}
     </GoogleMap>
   );
 }
