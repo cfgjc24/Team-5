69599b69b4b2454ce660ff769d5797391ce95749
JesseStuy20 <jxie20@stuy.edu>
2024-09-21 00:28:04 -0400

    Added routing on login page

--- client/src/login/Login.tsx
+++ client/src/login/Login.tsx
@@ -2,11 +2,35 @@ import React from "react";
 import { Input, Button } from "@nextui-org/react";
 import { EyeFilledIcon } from "./EyeFilledIcon";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom' ;
 
 export default function Login() {
   const [isVisible, setIsVisible] = React.useState<boolean>(false);
 
   const toggleVisibility = () => setIsVisible(!isVisible);
+  const [userName, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (userName == "supervisor") {
+      navigate("/supervisor") 
+    } ;
+    if (userName == "staff") {
+      navigate("/staff") 
+    } ;
+   
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center">
@@ -16,7 +40,7 @@ export default function Login() {
          <h1 className="text-2xl font-bold">Log In</h1>
 
         {/* Email Input */}
-        <Input
+        <Input onChange={handleUserNameChange}
           isRequired
           type="username"
           label="Username"
@@ -25,7 +49,7 @@ export default function Login() {
         />
 
         {/* Password Input */}
-        <Input
+        <Input onChange={handlePasswordChange}
           label="Password"
           variant="bordered"
           placeholder="Enter your password"
@@ -48,7 +72,7 @@ export default function Login() {
         />
 
         {/* Submit Button */}
-        <Button color="primary" size="md">
+        <Button color="primary" onClick = {handleSubmit} size="md">
           Log In
         </Button>
       </div>
