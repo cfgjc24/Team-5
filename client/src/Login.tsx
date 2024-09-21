c822b950cb1b6e54f92d548951d6e47120d48ed1
Emily Flores <110843762+efloresz@users.noreply.github.com>
2024-09-20 20:48:33 -0400

    updated to tsx

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 export default function Login() {
-  let [userName, setUserName] = useState<string>('')
-  let [password, setPassword] = useState<string>('')
+  const [userName, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleUserNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <>
-    <div>
-      </div>
+      <div></div>
       <h1>LoadStar</h1>
       <div className="card">
-    <h3>UserName:</h3> 
-    <input value={userName}></input>
-    <h3>Password:</h3>
-    <input value={password}></input>
-    </div>
-    <button>Login</button>
-
+        <h3>UserName:</h3>
+        <input value={userName} onChange={handleUserNameChange} />
+        <h3>Password:</h3>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <button>Login</button>
     </>
-  )
+  );
 }
-
-
