6c6800896cfaa02c08f94aef3725f903983ae86f
Emily Flores <e_floresrodriguez@massbay.edu>
2024-09-20 21:41:31 -0400

    added the tailwind imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './Login.tsx'
+import Login from './login/Login.tsx'
+import {NextUIProvider} from '@nextui-org/react'
+
 
 function App() {
   return (
     <>
-     <Login></Login> 
-     
+    <NextUIProvider><Login/></NextUIProvider>
     </>
   )
 }
---
+++ client/src/login/Login.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Input } from "@nextui-org/react";
+import { EyeFilledIcon } from "./EyeFilledIcon";
+import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+
+export default function Login() {
+  // Explicitly typing the state as a boolean
+  const [isVisible, setIsVisible] = React.useState<boolean>(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  return (
+    <div className="flex flex-col space-y-4">
+      {/* Email Input */}
+      <Input
+        isRequired
+        type="email"
+        label="Email"
+        defaultValue="junior@nextui.org"
+        className="max-w-xs"
+      />
+
+      {/* Password Input */}
+      <Input
+        label="Password"
+        variant="bordered"
+        placeholder="Enter your password"
+        endContent={
+          <button
+            className="focus:outline-none"
+            type="button"
+            onClick={toggleVisibility}
+            aria-label="toggle password visibility"
+          >
+            {isVisible ? (
+              <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+            ) : (
+              <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+            )}
+          </button>
+        }
+        // Toggling password visibility
+        type={isVisible ? "text" : "password"}
+        className="max-w-xs"
+        
+        
+      />
+    </div>
+    
+  );
+  
+}
+
