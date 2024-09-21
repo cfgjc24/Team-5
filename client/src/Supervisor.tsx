56680812f6092232fc388ad983f49029fa46db24
Xiaomin Liu <xl4624@nyu.edu>
2024-09-21 06:58:41 -0400

    Diya's changes

--- client/src/Supervisor.tsx
+++ client/src/Supervisor.tsx
@@ -4,16 +4,16 @@ import Sidebar from "./supervisor/components/Sidebar";
 
 export default function Supervisor() {
   return (
-    <div className="flex flex-row h-screen">
-      <div className="w-1/2 h-full">
+    <div className="flex flex-row h-[425px]">
+      <div className="w-1/2 h-full mx-4">
         <Map />
       </div>
 
-      <div className="flex flex-col w-1/2 h-full">
-        <div className="w-full h-1/2">
+      <div className="flex flex-col w-1/2 h-full mx-4">
+        <div className="w-full h-1/2 mb-2">
           <Notifications />
         </div>
-        <div className="w-full h-1/2">
+        <div className="w-full h-1/2 mb-2">
           <Sidebar />
         </div>
       </div>
